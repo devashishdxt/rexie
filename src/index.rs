944fb6ea37a22b45de558020a2b13e2c07c58711
Andreas Monitzer <a.monitzer@dungeonfog.com>
2022-04-01 21:42:58 +0200

    Added support for compound indices.

--- src/index.rs
+++ src/index.rs
@@ -1,3 +1,5 @@
+use js_sys::Array;
+use wasm_bindgen::JsValue;
 use web_sys::{IdbIndexParameters, IdbObjectStore};
 
 use crate::{Error, Result};
@@ -5,7 +7,7 @@ use crate::{Error, Result};
 /// An index builder.
 pub struct Index {
     pub(crate) name: String,
-    pub(crate) key_path: String,
+    pub(crate) key_path: Vec<String>,
     pub(crate) unique: Option<bool>,
     pub(crate) multi_entry: Option<bool>,
 }
@@ -15,7 +17,17 @@ impl Index {
     pub fn new(name: &str, key_path: &str) -> Self {
         Self {
             name: name.to_owned(),
-            key_path: key_path.to_owned(),
+            key_path: vec![key_path.to_owned()],
+            unique: None,
+            multi_entry: None,
+        }
+    }
+
+    /// Creates a new index with given name and compound key path
+    pub fn new_compound<S: ToString>(name: &str, key_path: impl IntoIterator<Item = S>) -> Self {
+        Self {
+            name: name.to_owned(),
+            key_path: key_path.into_iter().map(|s| s.to_string()).collect(),
             unique: None,
             multi_entry: None,
         }
@@ -47,9 +59,28 @@ impl Index {
                 params.multi_entry(multi_entry);
             }
 
-            object_store
-                .create_index_with_str_and_optional_parameters(&self.name, &self.key_path, &params)
-                .map_err(Error::IndexCreationFailed)?;
+            if self.key_path.len() == 1 {
+                object_store
+                    .create_index_with_str_and_optional_parameters(
+                        &self.name,
+                        &self.key_path[0],
+                        &params,
+                    )
+                    .map_err(Error::IndexCreationFailed)?;
+            } else {
+                object_store
+                    .create_index_with_str_sequence_and_optional_parameters(
+                        &self.name,
+                        &self
+                            .key_path
+                            .into_iter()
+                            .map(|s| JsValue::from_str(&s))
+                            .collect::<Array>()
+                            .into(),
+                        &params,
+                    )
+                    .map_err(Error::IndexCreationFailed)?;
+            }
         }
 
         Ok(())
